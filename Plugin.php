2d154e03fceb9ba0643fd2b2788027594f7f6a9e
WuHao <2323333339@qq.com>
2021-09-19 12:13:54 +0800

    更新简介

--- Plugin.php
+++ Plugin.php
@@ -2,7 +2,7 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 /**
- * A Comment Barrage For Typecho
+ * <a href="https://github.com/HaoOuBa/SMBarrage" target="_blank" rel="noopener noreferrer">A Comment Barrage For Typecho</a>
  * 
  * @package SMBarrage（什么弹幕）
  * @author Joe
